17e35a6329175f673fdf129f32274946b696ef5a
Kyqsx <kaiquicosta2@gmail.com>
2026-09-06 15:05:07 -0300

    Base ZipALink

---
+++ zipalink/src/test/java/com/kovax/zipalink/ZipalinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovax.zipalink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipalinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
